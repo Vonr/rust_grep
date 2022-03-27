094d1a1b6798d06d20ad03390541a1e9d0dc03ac
Vonr <dtaclanlegion@gmail.com>
2022-03-27 17:17:51 +0800

    Add -h, -v, and -F

--- README.md
+++ README.md
@@ -12,14 +12,29 @@ That being said, I do try my best to make the code as good as I know about.
 
 This project is also unlikely to receive any updates as it is already "finished" with all the features I care about.
 
+### Usage
+```
+Usage: rust_grep [OPTION]... PATTERN [FILES]...
+Search for PATTERN in FILES.
+Example:
+    rust_grep -i 'hello world' file.txt
+
+Options:
+-i          Ignore case distinctions in PATTERN
+-n          Print line number with output lines
+-v          Invert match: select non-matching lines
+-m <NUM>    Stop after NUM matches
+-h          Print this help and exit```
+```
+
 ### Features
 + Regex support
 + Customizable using command flags
     + Ignore casing with the -i flag
     + Show the line in which the match is found with the -n flag
     + Specify the maximum number of matches to be found with the -m <max\_num> flag
+    + String searching with -F flag (disables regex search)
+    + Inversion of pattern with -v flag
 
 ### Todo(?)
 + Colour (Low Priority)
-+ -F flag for string searching
-+ -v flag for inversion
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,195 @@
-use std::env::{self, Args};
+use std::{env::{self, Args}, process::exit};
 use regex::{Regex, RegexBuilder};
 
 struct Config {
-    pub query: Regex,
-    pub filename: String,
+    pub query: Option<Regex>,
+    pub filenames: Vec<String>,
     pub show_lines: bool,
     pub max: u32,
+    pub invert: bool,
+    pub string_search: Option<String>,
 }
 
 impl Config {
     fn new(args: Args) -> Config {
         let mut expecting_max = false;
         let mut query = String::new();
-        let mut filename = String::new();
+        let mut filenames: Vec<String> = Vec::new();
         let mut case_sensitive = true;
         let mut show_lines = false;
         let mut max = 0;
+        let mut invert = false;
+        let mut is_string_search = false;
 
         for arg in args.skip(1) {
             if arg.starts_with("-") {
                 let trimmed = arg.trim_start_matches("-");
-                match trimmed {
-                    "m" => expecting_max = true,
-                    _ => {
-                        for c in trimmed.chars() {
-                            match c {
-                                'i' => case_sensitive = false,
-                                'n' => show_lines = true,
-                                _ => panic!("Unrecognized flag: {}", c),
-                            }
+                if trimmed.starts_with("m=") {
+                    max = trimmed[2..].parse().unwrap_or(0);
+                    continue;
+                }
+                for c in trimmed.chars() {
+                    match c {
+                        'm' => expecting_max = true,
+                        'i' => case_sensitive = false,
+                        'n' => show_lines = true,
+                        'v' => invert = true,
+                        'F' => is_string_search = true,
+                        'h' => {
+                            print_help();
+                            exit(0);
+                        }
+                        _ => {
+                            eprintln!("Invalid flag: {}", c);
+                            print_help();
+                            exit(1);
                         }
                     }
                 }
             } else {
                 if expecting_max {
-                    max = arg.parse::<u32>().unwrap();
+                    max = arg.parse().unwrap_or(0);
                     expecting_max = false;
                 } else if query.is_empty() {
                     query = arg;
-                } else if filename.is_empty() {
-                    filename = arg;
                 } else {
-                    panic!("Too many arguments");
+                    filenames.push(arg);
                 }
             }
         }
 
-        let re = RegexBuilder::new(&query)
-            .case_insensitive(case_sensitive)
-            .build()
-            .expect("Invalid regex query provided");
-        Config {
-            query: re,
-            filename,
-            show_lines,
-            max,
+        if query.is_empty() {
+            eprintln!("No query specified");
+            print_help();
+            exit(1);
+        }
+
+        if filenames.len() == 0 {
+            eprintln!("No files specified");
+            print_help();
+            exit(1);
+        }
+
+        if is_string_search {
+            Config {
+                query: None,
+                filenames,
+                show_lines,
+                max,
+                invert,
+                string_search: Some(query),
+            }
+        } else {
+            let re = RegexBuilder::new(&query)
+                .case_insensitive(case_sensitive)
+                .build();
+
+            if re.is_err() {
+                eprintln!("Invalid regex {}: {}", query, re.err().unwrap());
+                print_help();
+                exit(1);
+            }
+
+            Config {
+                query: Some(re.unwrap()),
+                filenames,
+                show_lines,
+                max,
+                invert,
+                string_search: None,
+            }
         }
     }
+}
+
+fn print_help() {
+    println!("Usage: rust_grep [OPTION]... PATTERN [FILES]...\nSearch for PATTERN in FILES.\nExample:\n    rust_grep -i 'hello world' file.txt\n\nOptions:\n-i          Ignore case distinctions in PATTERN\n-n          Print line number with output lines\n-v          Invert match: select non-matching lines\n-m <NUM>    Stop after NUM matches\n-h          Print this help and exit")
 }
 
 fn main() {
     let cfg = Config::new(env::args());
 
-    print_matches(search(cfg.query, &cfg.filename, cfg.show_lines, cfg.max));
+    print_matches(search(&cfg.query, cfg.filenames, cfg.show_lines, cfg.max, cfg.invert, cfg.string_search));
 }
 
-fn search(query: Regex, filename: &str, show_lines: bool, max: u32) -> Vec<String> {
-    let content = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
+fn search(query: &Option<Regex>,
+          filenames: Vec<String>,
+          show_lines: bool,
+          max: u32,
+          invert: bool,
+          string_search: Option<String>)
+    -> Vec<String> {
 
-    let mut results = Vec::new();
-    for (i, line) in content.lines().enumerate() {
-        if query.is_match(line) {
-            results.push(
-                if show_lines {
-                    format!("{}:{}", i+1, line)
-                } else {
-                    line.to_string()
+        let multiple_files = filenames.len() > 1;
+        let mut results = Vec::new();
+
+        if let Some(string_search) = string_search {
+            for filename in filenames {
+                let mut matches: u32 = 0;
+                let content = read_file(&filename);
+
+                for (i, line) in content.lines().enumerate() {
+                    if line.contains(&string_search) ^ invert {
+                        results.push(format_line(i, &line, show_lines, &filename, multiple_files));
+                        matches += 1;
+                    }
+
+                    if max > 0 && matches >= max {
+                        break;
+                    }
                 }
-            );
+
+            }
+        } else if let Some(query) = query {
+            for filename in filenames {
+                let mut matches: u32 = 0;
+                let content = read_file(&filename);
+
+                for (i, line) in content.lines().enumerate() {
+                    if query.is_match(line) ^ invert {
+                        results.push(format_line(i, &line, show_lines, &filename, multiple_files));
+                        matches += 1;
+                    }
+
+                    if max > 0 && matches >= max {
+                        break;
+                    }
+                }
+            }
+        } else {
+            eprintln!("Invalid query provided");
+            print_help();
+            exit(1);
         }
 
-        if max > 0 && results.len() >= max as usize {
-            break;
-        }
+        results
     }
 
-    results
+fn format_line(index: usize, line: &str, show_lines: bool, filename: &str, multiple_files: bool) -> String {
+    if multiple_files {
+        if show_lines {
+            format!("{}:{}:{}", filename, index + 1, line)
+        } else {
+            format!("{}:{}", filename, line)
+        }
+    } else {
+        if show_lines {
+            format!("{}:{}", index + 1, line)
+        } else {
+            format!("{}", line)
+        }
+    }
+}
+
+fn read_file(filename: &str) -> String {
+    let content = std::fs::read_to_string(filename);
+    if content.is_err() {
+        eprintln!("Error reading {}: {}", filename, content.err().unwrap());
+        print_help();
+        exit(1);
+    }
+
+    content.unwrap()
 }
 
 fn print_matches(matches: Vec<String>) {
